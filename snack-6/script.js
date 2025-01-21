286f3712e4ad53eaba7cdeab244a4d827c7d39d0
Fede <fede.atzeni372@gmail.com>
2025-01-21 16:25:43 +0100

    feat snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,6 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+console.log(zucchine.filter((x) => x.length >= 15));
+
+console.log(zucchine.filter((x) => x.length < 15));
