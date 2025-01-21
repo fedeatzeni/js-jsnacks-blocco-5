90a179a229d2a43da2bbabbf33cd46864fb1fc9f
Fede <fede.atzeni372@gmail.com>
2025-01-21 16:10:35 +0100

    feat snack 4

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,5 +3,5 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
-const x = numbers.map((x) => x + 1);
-console.log(x);
+const y = numbers.map((x) => x + 1);
+console.log(y);
--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+console.log(posts.map((x) => x.author));
